e3ef53e4deb6f29f195b154f95585c0e3116345b
Paul Kinlan <paul.kinlan@gmail.com>
2018-01-15 16:41:37 +0000

    Fixes #57 - changes the proxy caching strategy

--- src/public/scripts/routes/proxy.js
+++ src/public/scripts/routes/proxy.js
@@ -2,33 +2,31 @@ import { loadData, fetch, caches, parseUrl, getProxyUrl, getProxyHeaders, paths,
 
 let config = loadData(`${paths.dataPath}config.json`).then(r => r.json());
 
-const proxyHandler = (request) => {
+const proxyHandler = (proxyRequest) => {
   /* 
     Go out to the networks.
   */ 
-  const url = parseUrl(request); // The URL we want to fetch.
+  const url = parseUrl(proxyRequest); // The URL we want to fetch.
 
   return config.then(c => {
     if(c.columns.map(col => col.feedUrl).indexOf(url) < 0 && url.endsWith('/all.rss') == false) {
-      // The request to proxy is not in the list of configured urls
+      // The proxyRequest to proxy is not in the list of configured urls
       return new Response("Proxy feed not configured", {status: "401"});
     }
     // Always hit the network, and update the cache so offline (and the streming) renders are ok.
-    return caches.match(request).then(response => {
-      let network = fetch(getProxyUrl(request), getProxyHeaders(request)).then(networkResponse => {    
-        if(networkResponse.ok) {
+    return caches.match(proxyRequest).then(cachedResponse => {
+      return fetch(getProxyUrl(proxyRequest), getProxyHeaders(proxyRequest)).then(fetchResponse => {    
+        if(fetchResponse.ok) {
+          // Update the cache, but return the network response
           return caches.open('data')
-              .then(cache => (!!cache) ? cache.put(request, networkResponse.clone()) : undefined)
-              .then(_ => networkResponse);
+              .then(cache => (!!cache) ? cache.put(proxyRequest, fetchResponse.clone()) : undefined)
+              .then(_ => fetchResponse);
+        }
+        else {
+          // The fetch has failed, just return from the cache.
+          return cachedResponse;
         }
       });
-
-      if(response === undefined) {
-        // If there is no match in the cache, just go to the network
-        return network;
-      }
-      // Otherwise, just race the two... response should win.
-      return Promise.race([response, network]);
     }).catch(error => {
       console.log("Proxy Fetch Error", error);
       throw error;
