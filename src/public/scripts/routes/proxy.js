803fb08470315592b5e4d0055ba4f94022885217
Paul Kinlan <paul.kinlan@gmail.com>
2018-01-15 14:49:28 +0000

    Stupid error. #42

--- src/public/scripts/routes/proxy.js
+++ src/public/scripts/routes/proxy.js
@@ -1,5 +1,4 @@
 import { loadData, fetch, caches, parseUrl, getProxyUrl, getProxyHeaders, paths, Response } from '../platform/common.js';
-import { getProxyHeaders } from '../platform/web';
 
 let config = loadData(`${paths.dataPath}config.json`).then(r => r.json());
 
