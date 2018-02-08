08f12ca3a9024dd84e4d417dd1f0372d5797aee9
Paul Kinlan <paul.kinlan@gmail.com>
2018-02-08 10:56:11 +0000

    Lint fixes for consitency

--- src/public/scripts/client.js
+++ src/public/scripts/client.js
@@ -1,88 +1,87 @@
-import { convertFeedItemsToJSON } from './data/common.js';
+import {convertFeedItemsToJSON} from './data/common.js';
 
-(function() { 
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope)
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err)
-      })
-    })
-  }
+(function() {
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
-  if('BroadcastChannel' in window) {
-    const installBroadcastChannel = new BroadcastChannel('install-cache-channel');
-    installBroadcastChannel.onmessage = (ev) => { 
-      console.log(ev);
-      const toast = document.getElementById('toast');
-      toast.classList.add('visible');
-      setTimeout(() => toast.classList.remove('visible'), 10000);
-    };
-  }
+if ('BroadcastChannel' in window) {
+  const installBroadcastChannel = new BroadcastChannel('install-cache-channel');
+  installBroadcastChannel.onmessage = (ev) => {
+    console.log(ev);
+    const toast = document.getElementById('toast');
+    toast.classList.add('visible');
+    setTimeout(() => toast.classList.remove('visible'), 10000);
+  };
+}
 
-  const applyTemplate = (templateElement, data) => {
-    const element = templateElement.content.cloneNode(true);    
-    const treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT, () => NodeFilter.FILTER_ACCEPT);
+const applyTemplate = (templateElement, data) => {
+  const element = templateElement.content.cloneNode(true);
+  const treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT, () => NodeFilter.FILTER_ACCEPT);
 
-    while(treeWalker.nextNode()) {
-      const node = treeWalker.currentNode;
-      for(let bindAttr in node.dataset) {
-        let isBindableAttr = (bindAttr.indexOf('bind_') == 0) ? true : false;
-        if(isBindableAttr) {
-          let dataKeyString = node.dataset[bindAttr];
-          let dataKeys = dataKeyString.split("|");
-          let bindKey = bindAttr.substr(5);
-          for(let dataKey of dataKeys) {
-            if(dataKey in data && data[dataKey] !== "") {
-              node[bindKey] = data[dataKey];
-              break;
-            }
+  while (treeWalker.nextNode()) {
+    const node = treeWalker.currentNode;
+    for (const bindAttr in node.dataset) {
+      const isBindableAttr = (bindAttr.indexOf('bind_') == 0) ? true : false;
+      if (isBindableAttr) {
+        const dataKeyString = node.dataset[bindAttr];
+        const dataKeys = dataKeyString.split('|');
+        const bindKey = bindAttr.substr(5);
+        for (const dataKey of dataKeys) {
+          if (dataKey in data && data[dataKey] !== '') {
+            node[bindKey] = data[dataKey];
+            break;
           }
         }
       }
     }
-
-    return element;
-  };
-
-  function waitForElement(selector, onElement) {
-    var elements = document.querySelectorAll(selector);
-
-    if(elements) {
-      onElement(Array.prototype.slice.call(elements));
-    }
-  
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        var nodes = Array.prototype.slice.call(mutation.addedNodes);
-        var matchedNodes = [];
-        for(var node of nodes) {
-          if(node.matches && node.nodeType == 1 && node.matches(selector)) {
-            matchedNodes.push(node);
-          }
-        };
-        onElement(matchedNodes);
-      });
-    });
-  
-    observer.observe(document.documentElement, { childList: true, subtree: true });
   }
 
-  waitForElement("section div[data-url]", columns => {
-  
-    const itemTemplate = document.getElementById('itemTemplate')
-    for(let column of columns) {
-      const feedUrl = column.dataset['url'];
-      const url = `/proxy?url=${encodeURIComponent(feedUrl)}`;
-      fetch(url)
+  return element;
+};
+
+function waitForElement(selector, onElement) {
+  const elements = document.querySelectorAll(selector);
+
+  if (elements) {
+    onElement(Array.prototype.slice.call(elements));
+  }
+
+  const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      let nodes = Array.prototype.slice.call(mutation.addedNodes);
+      let matchedNodes = [];
+      for (let node of nodes) {
+        if (node.matches && node.nodeType == 1 && node.matches(selector)) {
+          matchedNodes.push(node);
+        }
+      }
+      onElement(matchedNodes);
+    });
+  });
+
+  observer.observe(document.documentElement, {childList: true, subtree: true});
+}
+
+waitForElement('section div[data-url]', columns => {
+  const itemTemplate = document.getElementById('itemTemplate');
+  for (const column of columns) {
+    const feedUrl = column.dataset['url'];
+    const url = `/proxy?url=${encodeURIComponent(feedUrl)}`;
+    fetch(url)
         .then(feedResponse => {
-          let response = feedResponse.clone();
-          if('caches' in window) {
-            return caches.open('data').then(cache => { 
-              if(!!cache) {
+          const response = feedResponse.clone();
+          if ('caches' in window) {
+            return caches.open('data').then(cache => {
+              if (cache) {
                 cache.put(url, response);
               }
               return feedResponse.text();
@@ -92,73 +91,66 @@ import { convertFeedItemsToJSON } from './data/common.js';
         })
         .then(feedText => convertFeedItemsToJSON(feedText))
         .then(items => items.reverse())
-        .then(items => items.filter(item => !!!(document.getElementById(item.guid))))
+        .then(items => items.filter(item => !document.getElementById(item.guid)))
         .then(items => items.map(item => applyTemplate(itemTemplate.cloneNode(true), item)))
         .then(items => items.forEach(item => column.insertBefore(item, column.firstChild)));
-    }
-  });
-    
-  document.addEventListener('click', e => {
-    const target = e.target;
-    if(target.nodeName === 'svg' && target.classList.contains('share')) {
-      e.preventDefault();
-      const shareUrl = target.getAttribute("url") || "";
-      const shareTitle = target.getAttribute("title") || "";
-      if(!!navigator.share) {
-        navigator.share({
-          url: shareUrl,
-          title: shareTitle,
-          text: shareTitle
-        })
-      }
-      else {
-        let windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=520,height=420';
-        let twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(shareTitle)}&url=${encodeURIComponent(shareUrl)}&via=Paul_Kinlan`;
+  }
+});
 
-        window.open(twitterUrl, 'intent', windowOptions);
-      }
-    }
-  });
+document.addEventListener('click', e => {
+  const target = e.target;
+  if (target.nodeName === 'svg' && target.classList.contains('share')) {
+    e.preventDefault();
+    const shareUrl = target.getAttribute('url') || '';
+    const shareTitle = target.getAttribute('title') || '';
+    if (navigator.share) {
+      navigator.share({
+        url: shareUrl,
+        title: shareTitle,
+        text: shareTitle
+      });
+    } else {
+      const windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=520,height=420';
+      const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(shareTitle)}&url=${encodeURIComponent(shareUrl)}&via=Paul_Kinlan`;
 
-  document.addEventListener('keyup', e => {
-    const columns = document.querySelectorAll('section');
-    const selectedColumn = document.querySelector('section.selected');
-    let newSelectedColumn;
-    
-    if(e.code == 'ArrowLeft') {
-      //Move left
-      if(!!selectedColumn === false) {
+      window.open(twitterUrl, 'intent', windowOptions);
+    }
+  }
+});
+
+document.addEventListener('keyup', e => {
+  const columns = document.querySelectorAll('section');
+  const selectedColumn = document.querySelector('section.selected');
+  let newSelectedColumn;
+
+  if (e.code == 'ArrowLeft') {
+    // Move left
+    if (!!selectedColumn === false) {
+      newSelectedColumn = columns[columns.length - 1];
+    } else {
+      selectedColumn.classList.remove('selected');
+      if (selectedColumn.previousElementSibling) {
+        newSelectedColumn = selectedColumn.previousElementSibling;
+      } else {
         newSelectedColumn = columns[columns.length - 1];
       }
-      else {
-        selectedColumn.classList.remove('selected');
-        if(!!selectedColumn.previousElementSibling) {
-          newSelectedColumn = selectedColumn.previousElementSibling;
-        }
-        else {
-          newSelectedColumn = columns[columns.length - 1];
-        }
-      }
     }
-    else if(e.code == 'ArrowRight') {
-      //Move right
-      if(!!selectedColumn === false) {
+  } else if (e.code == 'ArrowRight') {
+    // Move right
+    if (!!selectedColumn === false) {
+      newSelectedColumn = columns[0];
+    } else {
+      selectedColumn.classList.remove('selected');
+      if (selectedColumn.nextElementSibling) {
+        newSelectedColumn = selectedColumn.nextElementSibling;
+      } else {
         newSelectedColumn = columns[0];
       }
-      else {
-        selectedColumn.classList.remove('selected');
-        if(!!selectedColumn.nextElementSibling) {
-          newSelectedColumn = selectedColumn.nextElementSibling;
-        }
-        else {
-          newSelectedColumn = columns[0];
-
-        }
-      }
     }
-    if(!!newSelectedColumn) {
-      newSelectedColumn.classList.add('selected');
-      newSelectedColumn.scrollIntoView({ "behaviour": "smooth" });
-    }
-  });
+  }
+  if (newSelectedColumn) {
+    newSelectedColumn.classList.add('selected');
+    newSelectedColumn.scrollIntoView({'behaviour': 'smooth'});
+  }
+});
 })();
--- src/public/sw.js
+++ src/public/sw.js
@@ -1,21 +1,25 @@
 /* VERSION: 0.0.91 */
-import { handler as root } from './scripts/routes/root.js';
-import { handler as proxy } from './scripts/routes/proxy.js';
-import { handler as all } from './scripts/routes/all.js';
+import {handler as root} from './scripts/routes/root.js';
+import {handler as proxy} from './scripts/routes/proxy.js';
+import {handler as all} from './scripts/routes/all.js';
 
-import { getCompiledTemplate, paths, generateCSPPolicy, generateIncrementalNonce } from './scripts/platform/common.js';
-import { WorkboxSW } from './scripts/workbox-sw.js';
-import { router } from './scripts/router.js';
+import {getCompiledTemplate,
+  paths,
+  generateCSPPolicy,
+  generateIncrementalNonce
+} from './scripts/platform/common.js';
+import {WorkboxSW} from './scripts/workbox-sw.js';
+import {router} from './scripts/router.js';
 
 
-const workbox = new WorkboxSW({ skipWaiting: true, precacheChannelName: 'install-cache-channel'});
+const workbox = new WorkboxSW({skipWaiting: true, precacheChannelName: 'install-cache-channel'});
 // This should pre-cache all of the required assets determined at buildtime.
 workbox.precache([]);
 
 getCompiledTemplate(`${paths.assetPath}templates/head.html`);
 
 const setHeader = (response, header, value) => {
-  response.headers.set(header, value)
+  response.headers.set(header, value);
   return response;
 };
 const generator = generateIncrementalNonce('service-worker');
@@ -27,7 +31,7 @@ const generator = generateIncrementalNonce('service-worker');
 // The proxy server '/proxy'
 
 router.get(`${self.location.origin}/proxy`, (e) => {
-  let response = proxy(e.request, {
+  const response = proxy(e.request, {
     dataPath: paths.dataPath,
     assetPath: paths.assetPath
   });
@@ -37,28 +41,28 @@ router.get(`${self.location.origin}/proxy`, (e) => {
 
 // The proxy server '/all'
 router.get(`${self.location.origin}/all$`, (e) => {
-  let nonce = {
+  const nonce = {
     analytics: generator(),
     inlinedcss: generator(),
     style: generator()
   };
 
-  let response = all(nonce, {
+  const response = all(nonce, {
     dataPath: paths.dataPath,
     assetPath: paths.assetPath
-  }).then(r => setHeader(r, 'Content-Security-Policy', generateCSPPolicy(nonce)));;
+  }).then(r => setHeader(r, 'Content-Security-Policy', generateCSPPolicy(nonce)));
   e.respondWith(response);
 }, {urlMatchProperty: 'href'});
 
 // The root '/'
 router.get(`${self.location.origin}/$`, (e) => {
-  let nonce = {
+  const nonce = {
     analytics: generator(),
     inlinedcss: generator(),
     style: generator()
   };
 
-  let response = root(nonce, {
+  const response = root(nonce, {
     dataPath: paths.dataPath,
     assetPath: paths.assetPath
   }).then(r => setHeader(r, 'Content-Security-Policy', generateCSPPolicy(nonce)));
